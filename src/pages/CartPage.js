427e0931dff1c7487bbd80539dac42c76d11c413
adhtanjung <adhtanjung@gmail.com>
2020-12-17 16:13:57 +0700

    ujian-frontend-adhitanjung

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button, Table } from "reactstrap";
+import { Button } from "reactstrap";
 import {
 	fetchCartAction,
 	deleteCartAction,
